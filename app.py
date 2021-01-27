7b9406d105c093ed52ad0de7d8f2657aee5e1c3b
Mingxun Wang <mwang87@gmail.com>
2021-01-27 10:38:09 -0800

    bump version

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ NAVBAR = dbc.Navbar(
 )
 
 DASHBOARD = [
-    dbc.CardHeader(html.H5("GNPS Dataset Dashboard - Version - 0.1")),
+    dbc.CardHeader(html.H5("GNPS Dataset Dashboard - Version - 0.2")),
     dbc.CardBody(
         [   
             dcc.Location(id='url', refresh=False),
