e744898217e57bdaac27fe5cff0aa12a086d03a4
Mingxun Wang <mwang87@gmail.com>
2023-08-16 16:53:50 -0700

    adding test

---
+++ test-integration/test.py
@@ -0,0 +1,17 @@
+import requests
+
+SERVER_URL = 'https://explorer.gnps2.org'
+
+def test_dataset():
+    url = SERVER_URL + '/api/datasets/MSV000086873/files'
+    r = requests.get(url)
+
+    print(r.json())
+    
+    r.raise_for_status()
+
+def main():
+    test_dataset()
+
+if __name__ == "__main__":
+    main()
