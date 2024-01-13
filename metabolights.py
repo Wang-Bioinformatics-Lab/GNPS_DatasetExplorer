7118236f1d399c8082aa7426c50799184f5ae4f0
YasinEl <yelabiead@ucsd.edu>
2024-01-12 23:17:10 -0800

    switched from samples to assay metadata

--- metabolights.py
+++ metabolights.py
@@ -3,6 +3,7 @@ import ftplib
 import sys
 import os
 import pandas as pd
+import re
 
 BASE_URL = 'https://www.ebi.ac.uk/metabolights/ws/studies'
 EBI_FTP_SERVER = 'ftp.ebi.ac.uk'
@@ -13,16 +14,68 @@ SWAGGER_API = 'https://www.ebi.ac.uk:443/metabolights/ws'
 def add_mtbls_metadata(files_df, accession):
     try:
         possible_assays, study_metadata_filename = get_active_assays(accession)
-        study_metadata_df = get_study_level_metadata(accession, study_metadata_filename)
+        assay_list = []
+        for assay_metadata in possible_assays:
+            study_metadata_df_single = get_study_level_metadata(accession, assay_metadata)
+            assay_list.append(study_metadata_df_single)
 
-        files_df["Sample Name"] = files_df["filename"].apply(lambda x: os.path.splitext(x)[0])
+        #add missing value columns
+        all_columns = set().union(*(df.columns for df in assay_list))
+        for df in assay_list:
+            for col in all_columns:
+                if col not in df.columns:
+                    df[col] = pd.NA
+
+        study_metadata_df = pd.concat(assay_list, ignore_index=True)     
+
+
+        # Remove unnecissary columns and adapt column names
+        def extract_bracketed_text(col_name):
+            match = re.search(r'\[(.*?)\]', col_name)
+            return match.group(1) if match else col_name
+        
+        study_metadata_df.columns = [extract_bracketed_text(col) for col in study_metadata_df.columns]
+        
+        
+        desired_columns = ['Instrument', 'Instrument manufacturer', 'Raw Spectral Data File', 'Derived Spectral Data File', 'Scan polarity']
+        study_metadata_df = study_metadata_df[[col for col in desired_columns if col in study_metadata_df.columns]]
+
+        # Duplicate rows if we have mzml AND raw files
+        df_study_raw = pd.DataFrame()
+        if 'Raw Spectral Data File' in study_metadata_df.columns:
+            study_metadata_df['Raw Spectral Data File'] = study_metadata_df['Raw Spectral Data File'].astype(str)
+            df_study_raw = study_metadata_df[study_metadata_df['Raw Spectral Data File'].str.contains('\.', regex=True, na=False)].copy()
+            df_study_raw['Sample Name'] = df_study_raw['Raw Spectral Data File']
+            df_study_raw.drop(columns=['Raw Spectral Data File', 'Derived Spectral Data File'], inplace=True)
+
+        df_study_mzml = pd.DataFrame()
+        if 'Derived Spectral Data File' in study_metadata_df.columns:
+            study_metadata_df['Derived Spectral Data File'] = study_metadata_df['Derived Spectral Data File'].astype(str)
+            df_study_mzml = study_metadata_df[study_metadata_df['Derived Spectral Data File'].str.contains('\.', regex=True, na=False)].copy()
+            df_study_mzml['Sample Name'] = df_study_mzml['Derived Spectral Data File']
+            df_study_mzml.drop(columns=['Raw Spectral Data File', 'Derived Spectral Data File'], inplace=True)
+
+        if len(df_study_raw) > 0 and len(df_study_mzml) > 0:
+            study_metadata_df = pd.concat([df_study_mzml, df_study_raw], ignore_index=True)
+        elif len(df_study_raw) > 0:
+            study_metadata_df = df_study_raw
+        elif len(df_study_mzml) > 0:
+            study_metadata_df = df_study_mzml
+        elif len(df_study_raw) == 0 and len(df_study_mzml) == 0:
+            return pd.DataFrame()
+
+
+        files_df["Sample Name"] = files_df["filename"]
         files_df = files_df.merge(study_metadata_df, how="left", on="Sample Name")
+
+
+        desired_columns = ['filename', 'Instrument', 'Instrument manufacturer', 'Scan polarity']
+        files_df = files_df[[col for col in desired_columns if col in files_df.columns]]
+
     except:
         pass
 
     return files_df
-
-
 
 def get_active_assays(study_id):
     url = '{SWAGGER_API}/studies/{study_id}/files?include_raw_data=false'.format(SWAGGER_API=SWAGGER_API, study_id=study_id)
